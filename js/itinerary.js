671c6c30959c00d16c5b4b001e6ebbdb1c807855
Wheesay <wizelim@gmail.com>
2022-08-02 15:11:24 +0800

    add click count and timer
    addition of timer and count with alert

--- js/itinerary.js
+++ js/itinerary.js
@@ -124,3 +124,36 @@ function arraymove(arr, fromIndex, toIndex) {
   arr.splice(fromIndex, 1);
   arr.splice(toIndex, 0, element);
 }
+/*watch*/
+
+var timerVar = setInterval(countTimer, 1000);
+var totalSeconds = 0;
+function countTimer() {
+           ++totalSeconds;
+           var hour = Math.floor(totalSeconds /3600);
+           var minute = Math.floor((totalSeconds - hour*3600)/60);
+           var seconds = totalSeconds - (hour*3600 + minute*60);
+           if(hour < 10)
+             hour = "0"+hour;
+           if(minute < 10)
+             minute = "0"+minute;
+           if(seconds < 10)
+             seconds = "0"+seconds;
+           document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+        }
+
+/* alert for topwatch*/
+function myAlert() {
+  alert("You have completed the study. Please remember to indicate the timing and the number of clicks as stated in the survey. Select OK to continue. ")
+  alert( " Total seconds taken : " + totalSeconds +" Total number of Clicks :  " + count )
+}
+/*count*/
+let count = 0;
+      let btn = document.querySelector('html');
+      let divSection = document.getElementById('showCount');
+      btn.addEventListener('click', (e)=>{
+        count++;
+        divSection.innerHTML=`Number of Clicks are: ${count}`;
+     });
+
+     
