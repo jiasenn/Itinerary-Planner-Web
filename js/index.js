8e1ffd89a06db0a24e34e7095df7e2eda9d154d2
jiasenn <jiasennn@gmail.com>
2022-08-03 19:16:48 +0800

    directions

--- js/index.js
+++ js/index.js
@@ -66,12 +66,17 @@ function initMap() {
 
   autocomplete.addListener("place_changed", function () {
     infowindow.close();
-    marker.setVisible(false);
+    // marker.setVisible(false);
     var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      window.alert("Autocomplete's returned place contains no geometry");
-      return;
-    }
+    // if (!place.place_id) {
+    //   alert("Please select an option from the dropdown list.");
+    //   return;
+    // }
+
+    // if (!place.geometry) {
+    //   window.alert("Autocomplete's returned place contains no geometry");
+    //   return;
+    // }
 
     // If the place has a geometry, then present it on a map.
     if (place.geometry.viewport) {
@@ -125,7 +130,114 @@ function initMap() {
     document.getElementById("lat").innerHTML = place.geometry.location.lat();
     document.getElementById("lon").innerHTML = place.geometry.location.lng();
   });
+  
+  new AutocompleteDirectionsHandler(map);
 }
+
+function AutocompleteDirectionsHandler(map) {
+  this.map = map;
+  this.originPlaceId = null;
+  this.destinationPlaceId = null;
+  this.travelMode = "WALKING";
+  var originInput = document.getElementById("origin-input");
+  var destinationInput = document.getElementById("destination-input");
+  var modeSelector = document.getElementById("mode-selector");
+  this.directionsService = new google.maps.DirectionsService();
+  this.directionsDisplay = new google.maps.DirectionsRenderer();
+  this.directionsDisplay.setMap(map);
+
+  var originAutocomplete = new google.maps.places.Autocomplete(
+    originInput,
+    { placeIdOnly: true }
+  );
+  var destinationAutocomplete = new google.maps.places.Autocomplete(
+    destinationInput,
+    { placeIdOnly: true }
+  );
+
+  this.setupClickListener("changemode-walking", "WALKING");
+  this.setupClickListener("changemode-transit", "TRANSIT");
+  this.setupClickListener("changemode-driving", "DRIVING");
+
+  this.setupPlaceChangedListener(originAutocomplete, "ORIG");
+  this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
+
+  // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+  // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+  // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+}
+
+// Sets a listener on a radio button to change the filter type on Places
+// Autocomplete.
+AutocompleteDirectionsHandler.prototype.setupClickListener = function (
+  id,
+  mode
+) {
+  var radioButton = document.getElementById(id);
+  var me = this;
+  radioButton.addEventListener("click", function () {
+    me.travelMode = mode;
+    me.route();
+  });
+};
+
+AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener =
+  function (autocomplete, mode) {
+    var me = this;
+    autocomplete.bindTo("bounds", this.map);
+    autocomplete.addListener("place_changed", function () {
+      var place = autocomplete.getPlace();
+      if (!place.place_id) {
+        window.alert("Please select an option from the dropdown list.");
+        return;
+      }
+      if (mode === "ORIG") {
+        me.originPlaceId = place.place_id;
+      } else {
+        me.destinationPlaceId = place.place_id;
+      }
+      me.route();
+    });
+  };
+
+AutocompleteDirectionsHandler.prototype.route = function () {
+  if (!this.originPlaceId || !this.destinationPlaceId) {
+    return;
+  }
+  var me = this;
+
+  // alert(this.originPlaceId);
+
+  this.directionsService.route(
+    {
+      origin: { placeId: this.originPlaceId },
+      destination: { placeId: this.destinationPlaceId },
+      travelMode: this.travelMode,
+    },
+    function (response, status) {
+      if (status === "OK") {
+        me.directionsDisplay.setDirections(response);
+
+        document.getElementById("startlat").innerHTML =
+          response.routes[0].legs[0].start_location.lat();
+        document.getElementById("startlng").innerHTML =
+          response.routes[0].legs[0].start_location.lng();
+        document.getElementById("endlat").innerHTML =
+          response.routes[0].legs[0].end_location.lat();
+        document.getElementById("endlng").innerHTML =
+          response.routes[0].legs[0].end_location.lng();
+
+        console.log(response.routes[0].legs[0].start_location.lat());
+        console.log(response.routes[0].legs[0].start_location.lng());
+
+        console.log(response.routes[0].legs[0].end_location.lat());
+        console.log(response.routes[0].legs[0].end_location.lng());
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+  );
+};
 
 function chooseDay(i) {
   input = document.getElementById("exactLoc" + String(i));
