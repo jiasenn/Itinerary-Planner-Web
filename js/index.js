dc2f5414759e0a212c5e653b9d09f9e4fe1339d7
jiasenn <jiasennn@gmail.com>
2022-08-03 12:32:25 +0800

    button adjustment

--- js/index.js
+++ js/index.js
@@ -53,8 +53,8 @@ function initMap() {
   map.panTo(marker.position);
   // console.log(map);
 
-  var input = document.getElementById("exactLoc");
-  // map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+  var input = document.getElementById("searchInput");
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   var autocomplete = new google.maps.places.Autocomplete(input);
   autocomplete.bindTo("bounds", map);
