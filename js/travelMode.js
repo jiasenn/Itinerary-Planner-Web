0be8c6faec3d1ee96b290c1f5b1e4719e8b4381c
jiasenn <jiasennn@gmail.com>
2022-08-06 01:27:15 +0800

    directions day each day fixed

---
+++ js/travelMode.js
@@ -0,0 +1,243 @@
+// DAY 1
+var header1 = document.getElementById("toggle1");
+var btns1 = header1.getElementsByClassName("btn1");
+for (var i = 0; i < btns1.length; i++) {
+  btns1[i].addEventListener("click", function () {
+    var current1 = document.getElementsByClassName("active");
+    current1[0].className = current1[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+var header2 = document.getElementById("toggle2");
+var btns2 = header2.getElementsByClassName("btn2");
+for (var i = 0; i < btns2.length; i++) {
+  btns2[i].addEventListener("click", function () {
+    var current2 = document.getElementsByClassName("active2");
+    console.log(current2[0]);
+    current2[0].className = current2[0].className.replace(" active2", "");
+    this.className += " active2";
+  });
+}
+var header3 = document.getElementById("toggle3");
+var btns3 = header3.getElementsByClassName("btn3");
+for (var i = 0; i < btns3.length; i++) {
+  btns3[i].addEventListener("click", function () {
+    var current3 = document.getElementsByClassName("active3");
+    console.log(current3[0]);
+    current3[0].className = current3[0].className.replace(" active3", "");
+    this.className += " active3";
+  });
+}
+var header4 = document.getElementById("toggle4");
+var btns4 = header4.getElementsByClassName("btn4");
+for (var i = 0; i < btns4.length; i++) {
+  btns4[i].addEventListener("click", function () {
+    var current4 = document.getElementsByClassName("active4");
+    console.log(current4[0]);
+    current4[0].className = current4[0].className.replace(" active4", "");
+    this.className += " active4";
+  });
+}
+var header5 = document.getElementById("toggle5");
+var btns5 = header5.getElementsByClassName("btn5");
+for (var i = 0; i < btns5.length; i++) {
+  btns5[i].addEventListener("click", function () {
+    var current5 = document.getElementsByClassName("active5");
+    console.log(current5[0]);
+    current5[0].className = current5[0].className.replace(" active5", "");
+    this.className += " active5";
+  });
+}
+var header6 = document.getElementById("toggle6");
+var btns6 = header6.getElementsByClassName("btn6");
+for (var i = 0; i < btns6.length; i++) {
+  btns6[i].addEventListener("click", function () {
+    var current6 = document.getElementsByClassName("active6");
+    console.log(current6[0]);
+    current6[0].className = current6[0].className.replace(" active6", "");
+    this.className += " active6";
+  });
+}
+
+// DAY 2
+var header21 = document.getElementById("toggle21");
+var btns21 = header21.getElementsByClassName("btn21");
+for (var i = 0; i < btns21.length; i++) {
+  btns21[i].addEventListener("click", function () {
+    var current21 = document.getElementsByClassName("active21");
+    current21[0].className = current21[0].className.replace(" active21", "");
+    this.className += " active21";
+  });
+}
+var header22 = document.getElementById("toggle2");
+var btns22 = header22.getElementsByClassName("btn2");
+for (var i = 0; i < btns22.length; i++) {
+  btns22[i].addEventListener("click", function () {
+    var current22 = document.getElementsByClassName("active22");
+    console.log(current22[0]);
+    current22[0].className = current22[0].className.replace(" active22", "");
+    this.className += " active22";
+  });
+}
+var header23 = document.getElementById("toggle23");
+var btns23 = header23.getElementsByClassName("btn23");
+for (var i = 0; i < btns23.length; i++) {
+  btns23[i].addEventListener("click", function () {
+    var current23 = document.getElementsByClassName("active23");
+    console.log(current23[0]);
+    current23[0].className = current23[0].className.replace(" active23", "");
+    this.className += " active23";
+  });
+}
+var header24 = document.getElementById("toggle24");
+var btns24 = header24.getElementsByClassName("btn24");
+for (var i = 0; i < btns24.length; i++) {
+  btns24[i].addEventListener("click", function () {
+    var current24 = document.getElementsByClassName("active24");
+    console.log(current24[0]);
+    current24[0].className = current24[0].className.replace(" active24", "");
+    this.className += " active24";
+  });
+}
+var header25 = document.getElementById("toggle25");
+var btns25 = header25.getElementsByClassName("btn25");
+for (var i = 0; i < btns25.length; i++) {
+  btns25[i].addEventListener("click", function () {
+    var current25 = document.getElementsByClassName("active25");
+    console.log(current25[0]);
+    current25[0].className = current25[0].className.replace(" active25", "");
+    this.className += " active25";
+  });
+}
+var header26 = document.getElementById("toggle26");
+var btns26 = header26.getElementsByClassName("btn26");
+for (var i = 0; i < btns26.length; i++) {
+  btns26[i].addEventListener("click", function () {
+    var current26 = document.getElementsByClassName("active26");
+    console.log(current26[0]);
+    current26[0].className = current26[0].className.replace(" active26", "");
+    this.className += " active26";
+  });
+}
+
+// DAY 3
+var header31 = document.getElementById("toggle31");
+var btns31 = header31.getElementsByClassName("btn31");
+for (var i = 0; i < btns31.length; i++) {
+  btns31[i].addEventListener("click", function () {
+    var current31 = document.getElementsByClassName("active31");
+    current31[0].className = current31[0].className.replace(" active31", "");
+    this.className += " active31";
+  });
+}
+var header32 = document.getElementById("toggle2");
+var btns32 = header32.getElementsByClassName("btn2");
+for (var i = 0; i < btns32.length; i++) {
+  btns32[i].addEventListener("click", function () {
+    var current32 = document.getElementsByClassName("active32");
+    console.log(current32[0]);
+    current32[0].className = current32[0].className.replace(" active32", "");
+    this.className += " active32";
+  });
+}
+var header233 = document.getElementById("toggle33");
+var btns33 = header33.getElementsByClassName("btn33");
+for (var i = 0; i < btns33.length; i++) {
+  btns33[i].addEventListener("click", function () {
+    var current33 = document.getElementsByClassName("active33");
+    console.log(current33[0]);
+    current33[0].className = current33[0].className.replace(" active33", "");
+    this.className += " active33";
+  });
+}
+var header34 = document.getElementById("toggle34");
+var btns34 = header34.getElementsByClassName("btn34");
+for (var i = 0; i < btns34.length; i++) {
+  btns34[i].addEventListener("click", function () {
+    var current34 = document.getElementsByClassName("active34");
+    console.log(current34[0]);
+    current34[0].className = current34[0].className.replace(" active34", "");
+    this.className += " active34";
+  });
+}
+var header35 = document.getElementById("toggle35");
+var btns35 = header35.getElementsByClassName("btn35");
+for (var i = 0; i < btns35.length; i++) {
+  btns35[i].addEventListener("click", function () {
+    var current35 = document.getElementsByClassName("active35");
+    console.log(current35[0]);
+    current35[0].className = current35[0].className.replace(" active35", "");
+    this.className += " active35";
+  });
+}
+var header36 = document.getElementById("toggle36");
+var btns36 = header36.getElementsByClassName("btn36");
+for (var i = 0; i < btns36.length; i++) {
+  btns36[i].addEventListener("click", function () {
+    var current36 = document.getElementsByClassName("active36");
+    console.log(current36[0]);
+    current36[0].className = current36[0].className.replace(" active36", "");
+    this.className += " active36";
+  });
+}
+
+// DAY 4
+var header41 = document.getElementById("toggle41");
+var btns41 = header41.getElementsByClassName("btn41");
+for (var i = 0; i < btns41.length; i++) {
+  btns41[i].addEventListener("click", function () {
+    var current41 = document.getElementsByClassName("active41");
+    current41[0].className = current41[0].className.replace(" active41", "");
+    this.className += " active41";
+  });
+}
+var header42 = document.getElementById("toggle2");
+var btns42 = header42.getElementsByClassName("btn2");
+for (var i = 0; i < btns42.length; i++) {
+  btns42[i].addEventListener("click", function () {
+    var current42 = document.getElementsByClassName("active42");
+    console.log(current42[0]);
+    current42[0].className = current42[0].className.replace(" active42", "");
+    this.className += " active42";
+  });
+}
+var header43 = document.getElementById("toggle43");
+var btns43 = header43.getElementsByClassName("btn43");
+for (var i = 0; i < btns43.length; i++) {
+  btns43[i].addEventListener("click", function () {
+    var current43 = document.getElementsByClassName("active43");
+    console.log(current43[0]);
+    current43[0].className = current43[0].className.replace(" active43", "");
+    this.className += " active43";
+  });
+}
+var header44 = document.getElementById("toggle44");
+var btns44 = header44.getElementsByClassName("btn44");
+for (var i = 0; i < btns44.length; i++) {
+  btns44[i].addEventListener("click", function () {
+    var current44 = document.getElementsByClassName("active44");
+    console.log(current44[0]);
+    current44[0].className = current44[0].className.replace(" active44", "");
+    this.className += " active44";
+  });
+}
+var header45 = document.getElementById("toggle45");
+var btns45 = header45.getElementsByClassName("btn45");
+for (var i = 0; i < btns45.length; i++) {
+  btns45[i].addEventListener("click", function () {
+    var current45 = document.getElementsByClassName("active45");
+    console.log(current45[0]);
+    current45[0].className = current45[0].className.replace(" active45", "");
+    this.className += " active45";
+  });
+}
+var header46 = document.getElementById("toggle46");
+var btns46 = header46.getElementsByClassName("btn46");
+for (var i = 0; i < btns46.length; i++) {
+  btns46[i].addEventListener("click", function () {
+    var current46 = document.getElementsByClassName("active46");
+    console.log(current46[0]);
+    current46[0].className = current46[0].className.replace(" active46", "");
+    this.className += " active46";
+  });
+}
