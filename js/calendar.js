04a8f6927d4950cf4274c0be84ee300c98ef094b
jiasenn <jiasennn@gmail.com>
2022-08-07 13:16:52 +0800

    remove draggable

--- js/calendar.js
+++ js/calendar.js
@@ -46,69 +46,69 @@ function hideCalendar() {
 
 
 // Almost final example
-(function() {
-  var id_ = 'draggable_table';
-  var cols_ = document.querySelectorAll('#' + id_ + ' .row');
-  var dragSrcEl_ = null;
+// (function() {
+//   var id_ = 'draggable_table';
+//   var cols_ = document.querySelectorAll('#' + id_ + ' .row');
+//   var dragSrcEl_ = null;
 
-  this.handleDragStart = function(e) {
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.innerHTML);
+//   this.handleDragStart = function(e) {
+//     e.dataTransfer.effectAllowed = 'move';
+//     e.dataTransfer.setData('text/html', this.innerHTML);
 
-    dragSrcEl_ = this;
+//     dragSrcEl_ = this;
 
-    this.style.opacity = '0.4';
+//     this.style.opacity = '0.4';
 
-  };
+//   };
 
-  this.handleDragOver = function(e) {
-    if (e.preventDefault) {
-      e.preventDefault(); // Allows us to drop.
-    }
+//   this.handleDragOver = function(e) {
+//     if (e.preventDefault) {
+//       e.preventDefault(); // Allows us to drop.
+//     }
 
-    e.dataTransfer.dropEffect = 'move';
+//     e.dataTransfer.dropEffect = 'move';
 
-    return false;
-  };
+//     return false;
+//   };
 
-  this.handleDragEnter = function(e) {
-  };
+//   this.handleDragEnter = function(e) {
+//   };
 
-  this.handleDragLeave = function(e) {
-  };
+//   this.handleDragLeave = function(e) {
+//   };
 
-  this.handleDrop = function(e) {
-    // this/e.target is current target element.
+//   this.handleDrop = function(e) {
+//     // this/e.target is current target element.
 
-    if (e.stopPropagation) {
-      e.stopPropagation(); // stops the browser from redirecting.
-    }
+//     if (e.stopPropagation) {
+//       e.stopPropagation(); // stops the browser from redirecting.
+//     }
 
-    // Don't do anything if we're dropping on the same column we're dragging.
-    if (dragSrcEl_ != this) {
-      dragSrcEl_.innerHTML = this.innerHTML;
-      this.innerHTML = e.dataTransfer.getData('text/html');
-    }
+//     // Don't do anything if we're dropping on the same column we're dragging.
+//     if (dragSrcEl_ != this) {
+//       dragSrcEl_.innerHTML = this.innerHTML;
+//       this.innerHTML = e.dataTransfer.getData('text/html');
+//     }
 
-    return false;
-  };
+//     return false;
+//   };
 
-  this.handleDragEnd = function(e) {
-    // this/e.target is the source node.
-    this.style.opacity = '1';
+//   this.handleDragEnd = function(e) {
+//     // this/e.target is the source node.
+//     this.style.opacity = '1';
 
-    [].forEach.call(cols_, function (col) {
-    });
-  };
+//     [].forEach.call(cols_, function (col) {
+//     });
+//   };
 
-  [].forEach.call(cols_, function (col) {
-    col.setAttribute('draggable', 'true');  // Enable columns to be draggable.
-    col.addEventListener('dragstart', this.handleDragStart, false);
-    col.addEventListener('dragenter', this.handleDragEnter, false);
-    col.addEventListener('dragover', this.handleDragOver, false);
-    col.addEventListener('dragleave', this.handleDragLeave, false);
-    col.addEventListener('drop', this.handleDrop, false);
-    col.addEventListener('dragend', this.handleDragEnd, false);
-  });
-})();
+//   [].forEach.call(cols_, function (col) {
+//     col.setAttribute('draggable', 'true');  // Enable columns to be draggable.
+//     col.addEventListener('dragstart', this.handleDragStart, false);
+//     col.addEventListener('dragenter', this.handleDragEnter, false);
+//     col.addEventListener('dragover', this.handleDragOver, false);
+//     col.addEventListener('dragleave', this.handleDragLeave, false);
+//     col.addEventListener('drop', this.handleDrop, false);
+//     col.addEventListener('dragend', this.handleDragEnd, false);
+//   });
+// })();
 
